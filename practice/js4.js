97fa1fd119bd49cadc990bf6b84287af2a1b2a64
Bohdan <bosrok.ke@gmail.com>
2025-07-02 12:53:03 +0100

    4module-1/5

---
+++ practice/js4.js
@@ -0,0 +1,21 @@
+const book = {
+  title: "The Last Kingdom",
+  author: "Bernand Cornwell",
+  genres: ["historical prose", "adventure"],
+  public: true,
+  rating: 8.38,
+};
+
+const user = {
+  name: "Jacques Gluke",
+  tag: "jgluke",
+  location: {
+    country: "Jamaica",
+    city: "Ocho Rios",
+  },
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
